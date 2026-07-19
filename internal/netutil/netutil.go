7e577386e4223f25948b0f5ba8be09556856031c
lucap9056 <jim12366219@gmail.com>
2026-07-19 22:18:15 +0800

    refactor(netutil): remove unused SendFDs/ReceiveFDs
    hub.go and node.go finished migrating to SendMsgWithFDs/
    ReceiveMsgWithFDs; no remaining callers (verified via grep).

--- internal/netutil/netutil.go
+++ internal/netutil/netutil.go
@@ -3,7 +3,6 @@
 package netutil
 
 import (
-	"fmt"
 	"net"
 
 	"golang.org/x/sys/unix"
@@ -18,43 +17,6 @@ const (
 	MsgHeartbeat MsgType = 3
 	MsgRelease   MsgType = 4
 )
-
-// SendFDs sends multiple file descriptors over a Unix Domain Socket.
-func SendFDs(conn net.Conn, fds ...int) error {
-	uc, ok := conn.(*net.UnixConn)
-	if !ok {
-		return fmt.Errorf("not a unix connection")
-	}
-
-	// We send a dummy byte with the OOB data
-	oob := unix.UnixRights(fds...)
-	_, _, err := uc.WriteMsgUnix([]byte{0}, oob, nil)
-	return err
-}
-
-// ReceiveFDs receives multiple file descriptors over a Unix Domain Socket.
-func ReceiveFDs(conn net.Conn, count int) ([]int, error) {
-	uc, ok := conn.(*net.UnixConn)
-	if !ok {
-		return nil, fmt.Errorf("not a unix connection")
-	}
-
-	buf := make([]byte, 1)
-	oob := make([]byte, unix.CmsgSpace(count*4))
-	n, oobn, _, _, err := uc.ReadMsgUnix(buf, oob)
-	if err != nil {
-		return nil, err
-	}
-	if n != 1 {
-		return nil, fmt.Errorf("failed to read data byte")
-	}
-
-	scm, err := unix.ParseSocketControlMessage(oob[:oobn])
-	if err != nil {
-		return nil, err
-	}
-	return unix.ParseUnixRights(&scm[0])
-}
 
 func SendMsgWithFDs(conn *net.UnixConn, msg []byte, fds ...int) error {
 	oob := unix.UnixRights(fds...)
