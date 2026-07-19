46e91ef3ec2239c821340b1f13b38c2b47b88d1d
lucap9056 <jim12366219@gmail.com>
2026-07-19 22:11:51 +0800

    feat(netutil): add batched SendMsgWithFDs/ReceiveMsgWithFDs
    Adds the ability to send arbitrary message bytes plus any number of
    FDs in a single WriteMsgUnix/ReadMsgUnix call, alongside the existing
    SendFDs/ReceiveFDs. Non-breaking: no caller changes yet.

--- internal/netutil/netutil.go
+++ internal/netutil/netutil.go
@@ -55,3 +55,40 @@ func ReceiveFDs(conn net.Conn, count int) ([]int, error) {
 	}
 	return unix.ParseUnixRights(&scm[0])
 }
+
+func SendMsgWithFDs(conn *net.UnixConn, msg []byte, fds ...int) error {
+	oob := unix.UnixRights(fds...)
+	_, _, err := conn.WriteMsgUnix(msg, oob, nil)
+	return err
+}
+
+func ReceiveMsgWithFDs(conn *net.UnixConn, msg []byte, oob []byte) (n int, fds []int, err error) {
+	n, oobn, _, _, err := conn.ReadMsgUnix(msg, oob)
+	if err != nil {
+		return n, nil, err
+	}
+
+	defer func() {
+		if err != nil {
+			for _, fd := range fds {
+				unix.Close(fd)
+			}
+			fds = nil
+		}
+	}()
+
+	scms, err := unix.ParseSocketControlMessage(oob[:oobn])
+	if err != nil {
+		return n, nil, err
+	}
+
+	for _, scm := range scms {
+		if scmFDs, err := unix.ParseUnixRights(&scm); err != nil {
+			return n, nil, err
+		} else {
+			fds = append(fds, scmFDs...)
+		}
+	}
+
+	return n, fds, nil
+}
